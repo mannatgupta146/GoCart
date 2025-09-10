1a79c3e03b30767fd343448d6eeb60f97d530147
mannatgupta146 <mannatgupta146@gmail.com>
2025-09-10 23:11:53 +0530

    ClerkProvider

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import { Outfit } from "next/font/google";
 import { Toaster } from "react-hot-toast";
 import StoreProvider from "@/app/StoreProvider";
+import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 
 const outfit = Outfit({ subsets: ["latin"], weight: ["400", "500", "600"] });
@@ -12,6 +13,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
     return (
+        <ClerkProvider>
         <html lang="en">
             <body className={`${outfit.className} antialiased`} suppressHydrationWarning={true}>
                 <StoreProvider>
@@ -20,5 +22,6 @@ export default function RootLayout({ children }) {
                 </StoreProvider>
             </body>
         </html>
+        </ClerkProvider>
     );
 }
