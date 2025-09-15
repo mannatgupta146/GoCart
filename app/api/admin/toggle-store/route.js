6d930b4ee0f044eee50bdc99ccf3a0b851b1b582
mannatgupta146 <mannatgupta146@gmail.com>
2025-09-15 16:04:30 +0530

    admin dashboard

--- app/api/admin/toggle-store/route.js
+++ app/api/admin/toggle-store/route.js
@@ -3,7 +3,6 @@ import authAdmin from "@/middlewares/authAdmin";
 import { getAuth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-
 // toggle store isActive
 export async function POST(request){
     try {
