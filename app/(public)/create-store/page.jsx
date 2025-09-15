e664e37b28eb82fb5c079a441b70e8458b7d1e0c
mannatgupta146 <mannatgupta146@gmail.com>
2025-09-15 14:26:06 +0530

    create store frontend

--- app/(public)/create-store/page.jsx
+++ app/(public)/create-store/page.jsx
@@ -4,9 +4,15 @@ import { useEffect, useState } from "react"
 import Image from "next/image"
 import toast from "react-hot-toast"
 import Loading from "@/components/Loading"
+import { useAuth, useUser } from "@clerk/nextjs"
+import { useRouter } from "next/navigation"
+import axios from "axios"
 
 export default function CreateStore() {
 
+    const {user} = useUser()
+    const router = useRouter()
+    const {getToken} = useAuth()
     const [alreadySubmitted, setAlreadySubmitted] = useState(false)
     const [status, setStatus] = useState("")
     const [loading, setLoading] = useState(true)
@@ -36,13 +42,42 @@ export default function CreateStore() {
     const onSubmitHandler = async (e) => {
         e.preventDefault()
         // Logic to submit the store details
+        if(!user){
+            return toast('Please login to continue')
+        }
+        try {
+            const token = await getToken()
+            const formData = new FormData()
+            formData.append("name", storeInfo.name)
+            formData.append("description", storeInfo.description)
+            formData.append("username", storeInfo.username)
+            formData.append("email", storeInfo.email)
+            formData.append("contact", storeInfo.contact)
+            formData.append("address", storeInfo.address)
+            formData.append("image", storeInfo.image)
 
+            const {data} = await axios.post('/api/store/create', formData, 
+                {headers: {Authorization: `Bearer ${token}`}}
+            )
+            toast.success(data.message)
+
+        } catch (error) {
+            toast.error(error?.response?.data?.error || error.message)
+        }
 
     }
 
     useEffect(() => {
         fetchSellerStatus()
     }, [])
+
+    if(!user){
+        return (
+            <div className="min-h-[80vh] mx-6 flex items-center justify-center text-slate-400">
+                <h1 className="text-2xl sm:text-4xl font-semibold">Please <span className="text-slate-500">login</span> to continue</h1>
+            </div>
+        )
+    }
 
     return !loading ? (
         <>
